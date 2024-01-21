e843121e54c2e55950fba24d34b5681c772e292a
vladimirgromozdin@gmail.com <vladimirgromozdin@Vladimirs-MacBook-Air.local>
2024-01-21 23:25:16 +0400

    hotfix: corrected types on the code

--- cypress/e2e/queue/button-clean.spec.cy.js
+++ cypress/e2e/queue/button-clean.spec.cy.js
@@ -1,12 +1,9 @@
 describe("cleaning the queue", function () {
-  it("should clean the quene and remove all eleemnts", function () {
+  it("should clean the queue and remove all elements", function () {
     cy.visit("http://localhost:3000/queue");
     cy.get('input[placeholder="Введите текст"]').as("queueInput");
     cy.get("button").contains("Добавить").closest("button").as("addButton");
     cy.get("button").contains("Очистить").closest("button").as("cleanButton");
-    cy.get('ul[class*="queue"] li:first')
-      .find('div[class*="circle_circle"]')
-      .as("firstElement");
     // Adding element to the queue
     cy.get("@queueInput").type("1234");
     cy.get("@addButton").click();
